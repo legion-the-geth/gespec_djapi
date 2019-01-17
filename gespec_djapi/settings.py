19c78cb3909b7f4090161bbbb4d33f2bcd8539f6
Meyniel Matthieu <matthieu.meyniel@gfi.fr>
2019-01-17 19:13:39 +0100

    models perimetre

--- gespec_djapi/settings.py
+++ gespec_djapi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'mouvement.apps.MouvementConfig',
+    'perimetre.apps.PerimetreConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'gespec_djapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gespec',
+        'USER': 'gespec',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- perimetre/models.py
+++ perimetre/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Perimetre(models.Model):
+    """
+    Définition du modèle d'un périmètre de conformité.
+    """
+    # Attributs propres
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    code = models.TextField(max_length=5, default=name[:5])
+    # Attributs liés
+    manager = models.ForeignKey('auth.User', related_name='perimetres', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, related_name='perimetres', on_delete=models.CASCADE)
+    annuaire = models.ForeignKey(Annuaire, related_name='perimetres', on_delete=models.SET(Annuaire.get_sentinel_ad))
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.code)
+
+    class Meta:
+        ordering = ('-last_modified', '-created')
+
+class Client(models.Model):
+    """
+    Définition du modèle d'un client
+    """
+    # Attributs propres
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('-last_modified', '-created')
+
+class Annuaire(models.Model):
+    """
+    Définition du modèle d'un Annuaire de type AD ou LDAP
+    """
+    # Attributs propres
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    host = models.CharField(max_length=100)
+    port = models.CharField(max_length=5)
+    user = models.CharField(max_length=50)
+    pw = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    def get_sentinel_ad():
+        return Annuaire.objects.get_or_create(name='default')[0]
+
+    class Meta:
+        ordering = ('-last_modified', '-created')
+
